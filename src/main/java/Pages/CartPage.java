b83e735c508583a1fd55487ed77518ef4df1986a
Rodaina Osama El Shorbagy <rodainaosama00@gmail.com>
2025-05-18 14:52:55 -0700

    Update CartPage.java

--- src/main/java/Pages/CartPage.java
+++ src/main/java/Pages/CartPage.java
@@ -3,6 +3,8 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.By;
+
 
 public class CartPage extends PageBase{
 
