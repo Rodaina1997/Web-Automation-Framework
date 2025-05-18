39d85b752b53a77a307fb8cd949efe1a05b581fc
Rodaina Osama El Shorbagy <rodainaosama00@gmail.com>
2025-05-18 14:29:41 -0700

    Update UserTests.java

--- src/test/java/tests/UserTests.java
+++ src/test/java/tests/UserTests.java
@@ -26,7 +26,7 @@ public class UserTests extends TestBase {
         homePage = new HomePage(driver);
         cartPage = new CartPage(driver);
         checkoutPage = new CheckoutPage(driver);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(100));
     }
 
 
@@ -40,40 +40,40 @@ public class UserTests extends TestBase {
 
 
     @Test(priority = 2, enabled = true)
-    public void UserCanAddBookToCart() {
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", homePage.addToBasketBtn);
-        wait.until(ExpectedConditions.elementToBeClickable(homePage.addToBasketBtn)).click();
-        driver.switchTo().defaultContent();
+    public void UserCanAddBookToCart()  {
+        // ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", homePage.addToBasketBtn);
+        wait.until(ExpectedConditions.elementToBeClickable(homePage.getAddToBasketBtn())).click();
     }
 
     @Story("Cart Page")
     @Severity(SeverityLevel.CRITICAL)
-    @Test(priority = 3, enabled = true )
-    public void UserCanViewCart() {
-        wait.until(ExpectedConditions.refreshed(ExpectedConditions.visibilityOf(homePage.cartBtn))).click();
-        System.out.println(homePage.cartBtn.getText());
+    @Test(priority = 3, enabled = true ,dependsOnMethods ={"UserCanAddBookToCart"})
+    public void UserCanViewCart() throws InterruptedException {
+      // Thread.sleep(100);
+        wait.until(ExpectedConditions.textToBePresentInElement(homePage.getCartButton(),"1"));
+        homePage.getCartButton().click();
+        //wait.until(ExpectedConditions.visibilityOf(homePage.getCartButton())).click();
     }
 
     @Story("Cart Items")
     @Severity(SeverityLevel.CRITICAL)
-    @Test(priority = 4, enabled = true)
+    @Test(priority = 4, enabled = true, dependsOnMethods = {"UserCanViewCart"})
     public void UserCanViewCartItems() {
-        wait.until(ExpectedConditions.visibilityOf(homePage.cartBtn));
-        Assert.assertEquals(cartPage.cartItem.getText(),"Thinking in HTML");
+        wait.until(ExpectedConditions.visibilityOf(homePage.getCartButton()));
+        Assert.assertEquals(cartPage.getCartItem().getText(),"Thinking in HTML");
     }
 
     @Story("Check-Out")
     @Severity(SeverityLevel.CRITICAL)
-    @Test(priority = 5, enabled = true)
+    @Test(priority = 5, enabled = true, dependsOnMethods = {"UserCanViewCartItems"})
     public void UserCanCheckout() {
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",cartPage.checkoutBtn);
-        wait.until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(cartPage.checkoutBtn)));
-        cartPage.checkoutBtn.click();
+        wait.until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(cartPage.getCheckoutButton())));
+        cartPage.getCheckoutButton().click();
     }
 
     @Story("Billing Details Form")
     @Severity(SeverityLevel.CRITICAL)
-    @Test(priority = 6, enabled = true)
+    @Test(priority = 6, enabled = true , dependsOnMethods = {"UserCanCheckout"})
     public void UserCanViewBillingDetailsForm() {
         Assert.assertTrue(checkoutPage.billingDetailsForm.isDisplayed());
         Assert.assertEquals(checkoutPage.billingDetailsTitle.getText(),"Billing Details");
