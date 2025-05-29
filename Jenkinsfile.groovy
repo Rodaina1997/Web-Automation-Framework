1cd92bab5abbe27207afb4c0c164b6dc5186d0f5
Rodaina1997 <rodainaosama00@gmail.com>
2025-05-29 16:34:11 +0300

    jenkins file commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,25 @@
+node {
+    def mvnHome
+    stage('Checkout') { // for display purposes
+        // Get some code from a GitHub repository
+        git branch: 'main', url: 'https://github.com/Rodaina1997/Web-Automation-Framework.git'
+        // Get the Maven tool.
+        // **       in the global configuration.
+        mvnHome = tool 'MAVEN_HOME'
+    }
+    stage('Build') {
+        // Run the maven build
+        withEnv(["MVN_HOME=$mvnHome"]) {
+            if (isUnix()) {
+                sh '"$MVN_HOME/bin/mvn" -Dmaven.test.failure.ignore clean package'
+            } else {
+                bat(/"%MVN_HOME%\bin\mvn" -Dmaven.test.failure.ignore clean package/)
+            }
+        }
+    }
+    stage('Results') {
+        junit '**/target/surefire-reports/TEST-*.xml'
+        archiveArtifacts 'target/*.jar'
+    }
+}
+
